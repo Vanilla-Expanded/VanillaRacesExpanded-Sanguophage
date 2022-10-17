f7401ed8bb3855ba6e15f9691eb1461247f14084
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-17 13:26:54 +0200

    More genes

---
+++ 1.4/Source/VanillaRacesExpanded-Sanguophage/VanillaRacesExpanded-Sanguophage/DefOfs/InternalDefOf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaRacesExpandedSanguophage
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+        public static HediffDef VRE_AnimalFed;
+
+       
+    }
+}
---
+++ 1.4/Source/VanillaRacesExpanded-Sanguophage/VanillaRacesExpanded-Sanguophage/Harmony/GeneGizmo_Resource_GizmoOnGUI.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+using RimWorld.Planet;
+
+
+
+namespace VanillaRacesExpandedSanguophage
+{
+
+
+    [HarmonyPatch(typeof(GeneGizmo_Resource))]
+    [HarmonyPatch("GizmoOnGUI")]
+
+    static class VanillaRacesExpandedSanguophage_GeneGizmo_Resource_GizmoOnGUI_Patch
+    {
+       
+
+        [HarmonyPrefix]
+        public static void ChangeBarColour(Gene ___gene, ref Texture2D ___barTex, ref Texture2D ___barHighlightTex)
+        {
+
+            if (___gene.pawn.health.hediffSet.GetFirstHediffOfDef(InternalDefOf.VRE_AnimalFed) != null) {
+                ___barTex = SolidColorMaterials.NewSolidColorTexture(new ColorInt(166, 90, 82).ToColor);
+                ___barHighlightTex = SolidColorMaterials.NewSolidColorTexture(new ColorInt(186, 117, 109).ToColor);
+            }
+                 
+
+
+
+        }
+
+
+    }
+   
+
+}
