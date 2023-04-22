fa62da19e4f80163d7fc86976f828681b9a48b69
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2023-04-22 18:44:02 +0200

    Make hemogen drain gizmo tooltip show doubled drain during blood moons
    I understand this is probably something rather rather insignificant that received a transpiler, so I wouldn't be surprised if it got rejected.
    
    I've patched GeneGizmo_ResourceHemogen.GetTooltip to:
    - Cache if the blood moon is active
    - Replace the call to IGeneResourceDrain.ResourceLossPerDay with our custom one
    - Inside of our custom replaced methods, we call IGeneResourceDrain.ResourceLossPerDay and return the value as doubled if the blood moon is active (based on the value that was cached earlier)
    
    I've tried to follow the same style as in the rest of the project, however if something doesn't match it - let me know and I'll change it. Same with any additional changes, if any are required.

---
+++ 1.4/Source/VanillaRacesExpanded-Sanguophage/VanillaRacesExpanded-Sanguophage/Harmony/GeneGizmo_ResourceHemogen_GetTooltip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+
+
+namespace VanillaRacesExpandedSanguophage
+{
+
+
+    [HarmonyPatch(typeof(GeneGizmo_ResourceHemogen))]
+    [HarmonyPatch("GetTooltip")]
+
+    static class GeneGizmo_ResourceHemogen_GetTooltip
+    {
+        private static bool CachedIsBloodMoon = false;
+
+        [HarmonyPrefix]
+        public static void CacheIsBloodMoon(Gene_Resource ___gene)
+        {
+            CachedIsBloodMoon = ___gene.pawn.Map?.GameConditionManager?.ConditionIsActive(InternalDefOf.VRE_BloodMoonCondition) == true;
+        }
+
+
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> ReplaceResourceLossPerDayCall(IEnumerable<CodeInstruction> instr)
+        {
+            MethodInfo target = AccessTools.DeclaredPropertyGetter(typeof(IGeneResourceDrain), nameof(IGeneResourceDrain.ResourceLossPerDay));
+            MethodInfo replacement = AccessTools.DeclaredMethod(typeof(GeneGizmo_ResourceHemogen_GetTooltip), nameof(DoubleResourceDrainDuringBloodmoon));
+
+            return instr.MethodReplacer(target, replacement);
+        }
+
+
+        public static float DoubleResourceDrainDuringBloodmoon(IGeneResourceDrain resource)
+        {
+            float loss = resource.ResourceLossPerDay;
+
+            if (CachedIsBloodMoon && loss > 0)
+                loss *= 2;
+            return loss;
+        }
+    }
+}
